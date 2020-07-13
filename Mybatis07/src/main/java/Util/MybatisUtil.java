0a7f2e0b35331beaf9302c54f89eada49694662c
Hjh8 <50100494+Hjh8@users.noreply.github.com>
2020-07-13 12:00:50 +0800

    第七章笔记

---
+++ Mybatis07/src/main/java/Util/MybatisUtil.java
@@ -0,0 +1,28 @@
+package Util;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MybatisUtil {
+    public static SqlSessionFactory factory;
+
+    static{
+        String config = "mybatis_conf.xml";
+        try {
+            InputStream in = Resources.getResourceAsStream(config);
+            factory = new SqlSessionFactoryBuilder().build(in);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+     public static SqlSession getSqlSession(){
+         return factory.openSession();
+     }
+}
--- Mybatis07/src/test/java/org/example/AppTest.java
+++ Mybatis07/src/test/java/org/example/AppTest.java
@@ -2,19 +2,34 @@ package org.example;
 
 import static org.junit.Assert.assertTrue;
 
+import Util.MybatisUtil;
+import com.github.pagehelper.PageHelper;
+import dao.StudentDao;
+import entity.Student;
+import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+import java.util.List;
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
+    public void TestJdbcProperties()
     {
-        assertTrue( true );
+        SqlSession sqlSession = MybatisUtil.getSqlSession();
+        StudentDao dao = sqlSession.getMapper(StudentDao.class);
+        List<Student> students = dao.selectAllStudent();
+        System.out.println(students);
+    }
+
+    @Test
+    public void TestPageHelper()
+    {
+        SqlSession sqlSession = MybatisUtil.getSqlSession();
+        StudentDao dao = sqlSession.getMapper(StudentDao.class);
+        // pageNum:第几页  pageSize：每页显示多少条数据
+        PageHelper.startPage(1,2);
+        List<Student> students = dao.selectAllStudent();
+        System.out.println(students);
     }
 }
